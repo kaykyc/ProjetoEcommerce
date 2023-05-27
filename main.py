13d2da58b1239782cc814078497297a9bdbd1989
Kayky Cordeiro <akc.silva@discente.ufma.br>
2023-05-27 00:48:30 -0300

    Add product commit

--- main.py
+++ main.py
@@ -2,14 +2,25 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+class Produto (BaseModel):
+    cod: int
+    descricao:str 
+    preco:float
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "World 2"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int,p: bool , q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post ("/Produto")
+def add_item(novo_produto: Produto):
+    return novo_produto
