4850abf153f823acfcfe5f25595426cee1c45a61
Kayky Cordeiro <akc.silva@discente.ufma.br>
2023-05-27 16:29:22 -0300

    Creating Headers and cookies

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import Union
+from typing import Optional, Union
 
-from fastapi import FastAPI
+from fastapi import Cookie, FastAPI, Header, Response
 
 from pydantic import BaseModel
 
@@ -13,13 +13,22 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World 2"}
+def read_root(user_agent: Optional[str] = Header(123)):
+    return {"Welcome to your store,"}
+    return{"user agente": user_agent}
 
+@app.get("/cookie")
+def cookie(response: Response):
+    response.set_cookie(key="meucookie", value="135566")
+    return {"cookie": True }
+
+@app.get("/get-cookie")
+def get_cookie(meucookie: Optional[str] = Cookie(None)):
+    return{"Cookie": meucookie}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int,p: bool , q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int,p: bool , q: Optional[str] = None):
+    return {"item_id": item_id, "q": q, "p":p}
 
 @app.post ("/Produto")
 def add_item(novo_produto: Produto):
